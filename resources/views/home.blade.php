bdf3a02ad9aaee08348f3181671c118d55da5236
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-12-11 23:53:27 +0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Welcome
                 </div>
             </div>
         </div>
