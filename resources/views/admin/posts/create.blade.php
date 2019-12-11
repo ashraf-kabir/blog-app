037a78a177011508412f9f560e03f301687a154e
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-12-12 00:14:46 +0600

    updates

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-center">
+        Create a new post
+    </h2>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/post/create', [
+    'uses' => 'PostsController@create',
+    'as' => 'post.create'
+]);
