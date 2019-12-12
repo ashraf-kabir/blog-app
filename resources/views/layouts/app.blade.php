f73e54035c86540ad08946d2ae5eb4dc132ef242
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-12-12 07:58:51 +0600

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,7 @@
                    <div class="col-lg-4">
                         <ul class="list-group">
                             <li class="list-group-item">
-                                <a href="/home">Home</a>
+                                <a href="{{ route('home') }}">Home</a>
                             </li>
                             <li class="list-group-item">
                                 <a href="{{ route('post.create') }}">Create new post</a>
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 
+    Route::get('/home', 'HomeController@index')->name('home');
+    
     Route::get('/post/create', [
         'uses' => 'PostsController@create',
         'as' => 'post.create'
