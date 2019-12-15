80b6a1d19bfbbf30ff9a3eade4911e8218aa1cc4
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-12-16 00:41:52 +0600

    updates

--- app/Post.php
+++ app/Post.php
@@ -3,13 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+
 
     protected $fillable = [
         'title','content','category_id','featured'
     ];
+
+    protected $dates = ['deleted_at'];
 
     public function category()
     {
