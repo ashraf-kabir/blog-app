2f0118b11b482f23a3a83bb2acb221f742035417
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-12-20 20:59:14 +0600

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,50 +52,50 @@
             <div class="col-lg-8">
                 <article class="hentry post post-standard has-post-thumbnail sticky">
 
-                        <div class="post-thumb">
-                            <img src="{{ $first_post->featured }}" alt="{{ $first_post->title }}">
-                            <div class="overlay"></div>
-                            <a href="{{ $first_post->featured }}" class="link-image js-zoom-image">
-                                <i class="seoicon-zoom"></i>
-                            </a>
-                            <a href="#" class="link-post">
-                                <i class="seoicon-link-bold"></i>
-                            </a>
+                    <div class="post-thumb">
+                        <img src="{{ $first_post->featured }}" alt="{{ $first_post->title }}">
+                        <div class="overlay"></div>
+                        <a href="{{ $first_post->featured }}" class="link-image js-zoom-image">
+                            <i class="seoicon-zoom"></i>
+                        </a>
+                        <a href="#" class="link-post">
+                            <i class="seoicon-link-bold"></i>
+                        </a>
+                    </div>
+
+                    <div class="post__content">
+
+                        <div class="post__content-info">
+
+                                <h2 class="post__title entry-title text-center">
+                                    <a href="{{ route('post.single', ['slug' => $first_post->slug ]) }}">{{ $first_post->title }}</a>
+                                </h2>
+
+                                <div class="post-additional-info">
+
+                                    <span class="post__date">
+
+                                        <i class="seoicon-clock"></i>
+
+                                        <time class="published" datetime="2016-04-17 12:00:00">
+                                            {{ $first_post->created_at->toFormattedDateString() }}
+                                        </time>
+
+                                    </span>
+
+                                    <span class="category">
+                                        <i class="seoicon-tags"></i>
+                                        <a href="{{ route('category.single', ['id' => $first_post->category->id]) }}}">{{ $first_post->category->name }}</a>
+                                    </span>
+
+                                    <span class="post__comments">
+                                        <a href="#"><i class="fa fa-comment-o" aria-hidden="true"></i></a>
+                                        6
+                                    </span>
+
+                                </div>
                         </div>
-
-                        <div class="post__content">
-
-                            <div class="post__content-info">
-
-                                    <h2 class="post__title entry-title text-center">
-                                        <a href="{{ route('post.single', ['slug' => $first_post->slug ]) }}">{{ $first_post->title }}</a>
-                                    </h2>
-
-                                    <div class="post-additional-info">
-
-                                        <span class="post__date">
-
-                                            <i class="seoicon-clock"></i>
-
-                                            <time class="published" datetime="2016-04-17 12:00:00">
-                                                {{ $first_post->created_at->toFormattedDateString() }}
-                                            </time>
-
-                                        </span>
-
-                                        <span class="category">
-                                            <i class="seoicon-tags"></i>
-                                            <a href="{{ route('category.single', ['id' => $first_post->category->id]) }}}">{{ $first_post->category->name }}</a>
-                                        </span>
-
-                                        <span class="post__comments">
-                                            <a href="#"><i class="fa fa-comment-o" aria-hidden="true"></i></a>
-                                            6
-                                        </span>
-
-                                    </div>
-                            </div>
-                        </div>
+                    </div>
 
                 </article>
             </div>
@@ -106,100 +106,100 @@
             <div class="col-lg-6">
                 <article class="hentry post post-standard has-post-thumbnail sticky">
 
-                        <div class="post-thumb">
-                            <img src="{{ $second_post->featured }}" alt="{{ $second_post->title }}">
-                            <div class="overlay"></div>
-                            <a href="{{ $second_post->featured }}" class="link-image js-zoom-image">
-                                <i class="seoicon-zoom"></i>
-                            </a>
-                            <a href="#" class="link-post">
-                                <i class="seoicon-link-bold"></i>
-                            </a>
-                        </div>
+                    <div class="post-thumb">
+                        <img src="{{ $second_post->featured }}" alt="{{ $second_post->title }}">
+                        <div class="overlay"></div>
+                        <a href="{{ $second_post->featured }}" class="link-image js-zoom-image">
+                            <i class="seoicon-zoom"></i>
+                        </a>
+                        <a href="#" class="link-post">
+                            <i class="seoicon-link-bold"></i>
+                        </a>
+                    </div>
 
-                        <div class="post__content">
+                    <div class="post__content">
 
-                            <div class="post__content-info">
+                        <div class="post__content-info">
 
-                                <h2 class="post__title entry-title text-center">
-                                    <a href="15_blog_details.html">{{ $second_post->title }}</a>
-                                </h2>
+                            <h2 class="post__title entry-title text-center">
+                                <a href="15_blog_details.html">{{ $second_post->title }}</a>
+                            </h2>
 
-                                <div class="post-additional-info">
+                            <div class="post-additional-info">
 
-                                    <span class="post__date">
+                                <span class="post__date">
 
-                                        <i class="seoicon-clock"></i>
+                                    <i class="seoicon-clock"></i>
 
-                                        <time class="published" datetime="2016-04-17 12:00:00">
-                                            {{ $second_post->created_at->toFormattedDateString() }}
-                                        </time>
+                                    <time class="published" datetime="2016-04-17 12:00:00">
+                                        {{ $second_post->created_at->toFormattedDateString() }}
+                                    </time>
 
-                                    </span>
+                                </span>
 
-                                    <span class="category">
-                                        <i class="seoicon-tags"></i>
-                                        <a href="{{ route('category.single', ['id' => $second_post->category->id ]) }}">{{ $second_post->category->name }}</a>
-                                    </span>
+                                <span class="category">
+                                    <i class="seoicon-tags"></i>
+                                    <a href="{{ route('category.single', ['id' => $second_post->category->id ]) }}">{{ $second_post->category->name }}</a>
+                                </span>
 
-                                    <span class="post__comments">
-                                        <a href="#"><i class="fa fa-comment-o" aria-hidden="true"></i></a>
-                                        6
-                                    </span>
+                                <span class="post__comments">
+                                    <a href="#"><i class="fa fa-comment-o" aria-hidden="true"></i></a>
+                                    6
+                                </span>
 
-                                </div>
                             </div>
                         </div>
+                    </div>
 
                 </article>
             </div>
             <div class="col-lg-6">
                 <article class="hentry post post-standard has-post-thumbnail sticky">
 
-                        <div class="post-thumb">
-                            <img src="{{ $third_post->featured }}" alt="{{ $third_post->title }}">
-                            <div class="overlay"></div>
-                            <a href="{{ $third_post->featured }}" class="link-image js-zoom-image">
-                                <i class="seoicon-zoom"></i>
-                            </a>
-                            <a href="#" class="link-post">
-                                <i class="seoicon-link-bold"></i>
-                            </a>
-                        </div>
+                    <div class="post-thumb">
+                        <img src="{{ $third_post->featured }}" alt="{{ $third_post->title }}">
+                        <div class="overlay"></div>
+                        <a href="{{ $third_post->featured }}" class="link-image js-zoom-image">
+                            <i class="seoicon-zoom"></i>
+                        </a>
+                        <a href="#" class="link-post">
+                            <i class="seoicon-link-bold"></i>
+                        </a>
+                    </div>
 
-                        <div class="post__content">
+                    <div class="post__content">
 
-                            <div class="post__content-info">
+                        <div class="post__content-info">
 
-                                <h2 class="post__title entry-title text-center">
-                                    <a href="15_blog_details.html">{{ $third_post->title }}</a>
-                                </h2>
+                            <h2 class="post__title entry-title text-center">
+                                <a href="15_blog_details.html">{{ $third_post->title }}</a>
+                            </h2>
 
-                                <div class="post-additional-info">
+                            <div class="post-additional-info">
 
-                                    <span class="post__date">
+                                <span class="post__date">
 
-                                        <i class="seoicon-clock"></i>
+                                    <i class="seoicon-clock"></i>
 
-                                        <time class="published" datetime="2016-04-17 12:00:00">
-                                            {{ $third_post->created_at->toFormattedDateString() }}
-                                        </time>
+                                    <time class="published" datetime="2016-04-17 12:00:00">
+                                        {{ $third_post->created_at->toFormattedDateString() }}
+                                    </time>
 
-                                    </span>
+                                </span>
 
-                                    <span class="category">
-                                        <i class="seoicon-tags"></i>
-                                        <a href="{{ route('category.single', ['id' => $third_post->category->id ]) }}">{{ $third_post->category->name }}</a>
-                                    </span>
+                                <span class="category">
+                                    <i class="seoicon-tags"></i>
+                                    <a href="{{ route('category.single', ['id' => $third_post->category->id ]) }}">{{ $third_post->category->name }}</a>
+                                </span>
 
-                                    <span class="post__comments">
-                                        <a href="#"><i class="fa fa-comment-o" aria-hidden="true"></i></a>
-                                        6
-                                    </span>
+                                <span class="post__comments">
+                                    <a href="#"><i class="fa fa-comment-o" aria-hidden="true"></i></a>
+                                    6
+                                </span>
 
-                                </div>
                             </div>
                         </div>
+                    </div>
 
                 </article>
             </div>
