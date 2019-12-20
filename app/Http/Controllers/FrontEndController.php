f331d71bc0b199a41d4098a93625937a6485d328
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-12-20 19:22:43 +0600

    blog details added

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -21,4 +21,13 @@ class FrontEndController extends Controller
                 ->with('technology', Category::find(5))
                 ->with('settings', Setting::first());
     }
+
+    public function singlePost($slug) {
+        $post = Post::where('slug', $slug)->first();
+
+        return view('single')->with('post', $post)
+                            ->with('title', $post->title)
+                            ->with('settings', Setting::first())
+                            ->with('categories', Category::take(5)->get());
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::get('/', [
     'as' => 'index'
 ]);
 
+Route::get('/{slug}', [
+    'uses' => 'FrontEndController@singlePost',
+    'as' => 'post.single'
+]);
+
 Auth::routes();
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
