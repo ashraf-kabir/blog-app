867ca5a53a3ef239ea5a62782e09e6acdaa8adf3
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-12-21 19:03:15 +0600

    search updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -384,21 +384,7 @@
 
 <!-- Overlay Search -->
 
-<div class="overlay_search">
-    <div class="container">
-        <div class="row">
-            <div class="form_search-wrap">
-                <form method="GET" action="/results">
-                    <input class="overlay_search-input" name="query" placeholder="Type and hit enter..." type="text">
-                    <a href="#" class="overlay_search-close">
-                        <span></span>
-                        <span></span>
-                    </a>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+@include('includes.search')
 
 <!-- End Overlay Search -->
 
