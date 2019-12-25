c132a2cd2c497778861b8b43a3bb64e46feb23bc
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-12-25 15:31:01 +0600

    Update single.blade.php

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -27,7 +27,7 @@
                                     Posted by
     
                                     <div class="post__author-name fn">
-                                        <a href="#" class="post__author-link">Admin</a>
+                                        <a href="#" class="post__author-link">{{ $post->user->name }}</a>
                                     </div>
     
                                 </div>
